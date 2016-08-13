b9c9c928c54e48dbe7b16dd377b46197ee2efa98
Craig Doremus <cdoremus@gmail.com>
2016-08-13 15:32:36 -0400

    Updated test API

--- autocompletion/src/app/about/about.component.spec.ts
+++ autocompletion/src/app/about/about.component.spec.ts
@@ -1,24 +1,27 @@
-import {
-  async,
-  inject,
-  addProviders
-} from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { TestComponentBuilder } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
 describe('About Component', () => {
+  const html = '<my-about></my-about>';
+
   beforeEach(() => {
-    addProviders([]);
+    TestBed.configureTestingModule({declarations: [AboutComponent, TestComponent]});
+    TestBed.overrideComponent(TestComponent, { set: { template: html }});
   });
 
-  it('template should contain <p> with "About Works!" text', async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-    tcb.createAsync(AboutComponent).then((fixture) => {
-      fixture.detectChanges();
-      let compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('p').textContent.toString().trim()).toEqual('About Works!');
-    });
-  })));
+  it('template should contain "About Works!" text', () => {
+
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    expect(fixture.nativeElement.children[0].textContent).toContain('About Works!');
+
+  });
 
 });
+
+@Component({selector: 'test-cmp', template: ''})
+class TestComponent { }
+
