a26951451f19d0a62e44e1502aa8f50964f7a9c4
Craig Doremus <cdoremus@gmail.com>
2016-09-05 15:21:42 -0400

    Fixed test using try-catch

--- dynamic-form/src/app/home/home.component.spec.ts
+++ dynamic-form/src/app/home/home.component.spec.ts
@@ -1,22 +1,34 @@
 // This shows a different way of testing a component, check about for a simpler one
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { QuestionFormComponent } from '../questions/questions-form.component';
 
 describe('Home Component', () => {
   const html = '<my-home></my-home>';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [HomeComponent, TestComponent]});
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule], // used by QuestionFormComponent
+      declarations: [HomeComponent, QuestionFormComponent, TestComponent]}
+      );
+    // Component to contain the HomeComponent for testing
     TestBed.overrideComponent(TestComponent, { set: { template: html }});
   });
 
-  it('should ...', () => {
-    const fixture = TestBed.createComponent(TestComponent);
-    fixture.detectChanges();
-    expect(fixture.nativeElement.children[0].textContent).toContain('Home Works!');
+  it('should display "Please Enter Questions" from QuestionFormComponent template', () => {
+    try {
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      const element = fixture.nativeElement;
+      expect(element.children[0].textContent).toContain('Please Enter Questions');
+    } catch (err) {
+      // this is needed to debug any errors
+      console.log("Problem: " + err.message);
+    }
   });
 
 });
