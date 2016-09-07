877fe66ae7bac866142f5420f33ebf8fb76fab80
Craig Doremus <cdoremus@gmail.com>
2016-09-07 17:14:14 -0400

    Added popup dialog

--- routing/src/app/app.routing.ts
+++ routing/src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'dialog-path', component: DialogComponent, outlet: 'dialog' }
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- routing/src/app/dialog/dialog.component.ts
+++ routing/src/app/dialog/dialog.component.ts
@@ -1,25 +1,42 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-dialog',
   styles: [`
     .dialog-container {
       height: 400px;
-      length: 400px;
+      border: 3px solid black;
+      margin: 5px 250px;
+      box-shadow: 10px 10px 5px lightblue;
+      z-index: 1000;
+      position: relative;
     }
     .dismiss-button {
       margin: 30px auto;
+    }
+    button {
+      position: absolute;
+      bottom: 10px;
+      left: 260px;
     }
   `],
   template: `
     <div class="dialog-container">
       <h2>Angular 2 Dialog</h2>
       <div class="dimiss-button">
-        <button>Dismiss Dialog</button>
+        <button (click)="closeDialog()">Dismiss Dialog</button>
       </div>
     </div>
   `
 })
 export class DialogComponent {
+
+  constructor(private router: Router) {}
+
+  closeDialog() {
+    console.log('Closing dialog');
+
+    this.router.navigate(['/']);
+  }
 }
