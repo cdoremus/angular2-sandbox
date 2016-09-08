50cf4e5345c464abdbcac6e7328beec5105c4437
Craig Doremus <cdoremus@gmail.com>
2016-09-08 13:02:26 -0400

    Formatted dialog better; added link to home

--- routing/src/app/dialog/dialog.component.ts
+++ routing/src/app/dialog/dialog.component.ts
@@ -5,25 +5,36 @@ import { Router } from '@angular/router';
   selector: 'my-dialog',
   styles: [`
     .dialog-container {
-      height: 400px;
+      height: 300px;
+      width: 300px;
       border: 3px solid black;
-      margin: 5px 250px;
+      background-color: #0002ff;
       box-shadow: 10px 10px 5px lightblue;
       z-index: 1000;
-      position: relative;
+      position: fixed;
+      top: 20%;
+      left: 40%;
+      opacity:  1;
+    }
+    .dialog-content {
+      margin: 20% 10%;
     }
     .dismiss-button {
-      margin: 30px auto;
     }
     button {
       position: absolute;
-      bottom: 10px;
-      left: 260px;
+      bottom: 10%;
+      left: 35%;
     }
   `],
   template: `
     <div class="dialog-container">
       <h2>Angular 2 Dialog</h2>
+      <div class="dialog-content">
+        This is a dialog displayed using the
+        <i>DialogComponent</i> class, a
+        named router-outlet tag and the routerLink directive.
+      </div>
       <div class="dimiss-button">
         <button (click)="closeDialog()">Dismiss Dialog</button>
       </div>
