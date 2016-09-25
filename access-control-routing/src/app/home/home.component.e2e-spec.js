7eb4732e43858bddd767905b6071f2aed6d46b78
Craig Doremus <cdoremus@gmail.com>
2016-09-25 11:13:53 -0400

    Fixed test but 2x login fails it

--- access-control-routing/src/app/home/home.component.e2e-spec.js
+++ access-control-routing/src/app/home/home.component.e2e-spec.js
@@ -1,10 +1,23 @@
 describe('Home', function () {
+  // valid login credentials
+  var luser = 'user';
+  var lpass = 'pass';
 
   beforeEach(function () {
     browser.get('/');
   });
 
-  it('should have <my-home>', function () {
+  it('should have <my-home> with "Home Works!" text after login', function () {
+    // guard redirects to login component
+    // login
+    var username = element(by.css("input[name='username']"));
+    username.sendKeys(luser);
+    var password = element(by.css("input[name='password']"));
+    password.sendKeys(lpass);
+    var login = element(by.css("input[type='submit']"));
+    login.click();
+    // login.click(); //should not need to click login twice
+    // redirected to home page after login
     var home = element(by.css('my-app my-home'));
     expect(home.isPresent()).toEqual(true);
     expect(home.getText()).toEqual("Home Works!");
