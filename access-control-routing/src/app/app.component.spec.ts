9060653e103eb4a6da1565a582875a46be9d11b9
Craig Doremus <cdoremus@gmail.com>
2016-09-25 11:50:08 -0400

    Fixed broken test due ot missing config

--- access-control-routing/src/app/app.component.spec.ts
+++ access-control-routing/src/app/app.component.spec.ts
@@ -1,21 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ApiService } from './shared';
 import { AppComponent } from './app.component';
+import { AuthenticationTokenProvider } from './login/authentication-token';
+import { LoginLinkComponent } from './login/login-link.component';
+
+class MockTokenProvider implements AuthenticationTokenProvider {
+  getToken(): string {
+    return 'foo';
+  }
+  setToken(token: string) {
+    ; //noop
+  }
+  removeToken() {
+    ; // noop
+  }
+}
 
 describe('App', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-      providers: [ApiService, provideRoutes([])]
+      imports: [BrowserModule, RouterTestingModule],
+      declarations: [LoginLinkComponent, AppComponent],
+      providers: [
+        ApiService,
+        provideRoutes([]),
+        {provide: AuthenticationTokenProvider, useClass: MockTokenProvider}
+        ]
     });
+    TestBed.compileComponents(); // useful for debugging messages it provides
   });
 
   it('should have an url', () => {
+
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     expect(fixture.debugElement.componentInstance.url).toEqual('https://github.com/preboot/angular2-webpack');
