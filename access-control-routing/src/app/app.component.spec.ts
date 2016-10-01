eb25822bd924d97e670f26a34782d9d2f8a64ea7
Craig Doremus <cdoremus@gmail.com>
2016-10-01 12:13:27 -0400

    Added modules to prevent NoProviderError

--- access-control-routing/src/app/app.component.spec.ts
+++ access-control-routing/src/app/app.component.spec.ts
@@ -2,44 +2,54 @@ import { TestBed } from '@angular/core/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideRoutes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { ApiService } from './shared';
 import { AppComponent } from './app.component';
 import { AuthenticationTokenProvider } from './login/authentication-token';
 import { LoginLinkComponent } from './login/login-link.component';
+import { LoginLinkService } from './login/login-link.service';
+import { LoginService } from './login/login.service';
 
 class MockTokenProvider implements AuthenticationTokenProvider {
   getToken(): string {
     return 'foo';
   }
   setToken(token: string) {
-    ; //noop
+    ; // noop
   }
   removeToken() {
     ; // noop
   }
 }
 
-describe('App', () => {
+describe('App Component', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [BrowserModule, RouterTestingModule],
+      imports: [BrowserModule, RouterTestingModule, HttpModule, FormsModule],
       declarations: [LoginLinkComponent, AppComponent],
       providers: [
         ApiService,
-        provideRoutes([]),
-        {provide: AuthenticationTokenProvider, useClass: MockTokenProvider}
+        LoginService,
+        LoginLinkService,
+        {provide: AuthenticationTokenProvider, useClass: MockTokenProvider},
+        provideRoutes([])
         ]
     });
     TestBed.compileComponents(); // useful for debugging messages it provides
   });
 
-  it('should have an url', () => {
+  it('should have a url', () => {
+    try {
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      expect(fixture.debugElement.componentInstance.url).toEqual('https://github.com/preboot/angular2-webpack');
+    } catch (error) {
+      console.log("App Component Error:", error);
+    }
 
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    expect(fixture.debugElement.componentInstance.url).toEqual('https://github.com/preboot/angular2-webpack');
   });
 
 });
