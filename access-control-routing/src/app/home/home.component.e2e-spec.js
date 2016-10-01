581712f4c3cf910427e06e7e69c446d2def63420
Craig Doremus <cdoremus@gmail.com>
2016-10-01 01:20:58 -0400

    Added expectations about login menu link

--- access-control-routing/src/app/home/home.component.e2e-spec.js
+++ access-control-routing/src/app/home/home.component.e2e-spec.js
@@ -7,7 +7,11 @@ describe('Home', function () {
     browser.get('/');
   });
 
-  it('should be able to login, redirected to home page and then to about page after About link is clicked', function () {
+  it('should be able to login, redirected to home page and then to about page after About link is clicked and have Login link text in menu change to Logout', function () {
+    //check login link text in menu
+    var loginLink = element(by.css('.login-link'));
+    expect(loginLink.getText()).toEqual("Login");
+
     // guard redirects to login component
     // login
     var username = element(by.css("input[name='username']"));
@@ -15,8 +19,9 @@ describe('Home', function () {
     var password = element(by.css("input[name='password']"));
     password.sendKeys(lpass);
     var login = element(by.css("input[type='submit']"));
+    // click Login button
     login.click();
-    // login.click(); //should not need to click login twice
+
     // redirected to home page after login
     var home = element(by.css('my-app my-home'));
     expect(home.isPresent()).toEqual(true);
@@ -28,6 +33,10 @@ describe('Home', function () {
     var about = element(by.css('my-app my-about'));
     expect(about.isPresent()).toEqual(true);
     expect(about.getText()).toEqual("About Works!");
+
+    // make sure that login link text === Logout
+    expect(loginLink.getText()).toEqual("Logout");
+
   });
 
 });
