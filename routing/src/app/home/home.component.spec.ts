34a4555180022cd4f32420f6c694a9f296a37d39
Craig Doremus <cdoremus@gmail.com>
2016-10-01 19:57:54 -0400

    Fixed test by importing RouterTestingModule

--- routing/src/app/home/home.component.spec.ts
+++ routing/src/app/home/home.component.spec.ts
@@ -1,7 +1,9 @@
 // This shows a different way of testing a component, check about for a simpler one
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -9,15 +11,20 @@ describe('Home Component', () => {
   const html = '<my-home></my-home>';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [HomeComponent, TestComponent]});
+    TestBed.configureTestingModule({
+      imports: [BrowserModule, FormsModule, RouterTestingModule ],
+      declarations: [HomeComponent, TestComponent]
+    });
     TestBed.overrideComponent(TestComponent, { set: { template: html }});
+    TestBed.compileComponents();
   });
 
-  it('should ...', () => {
+  it('should display "Home Works!"', () => {
     const fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
     expect(fixture.nativeElement.children[0].textContent).toContain('Home Works!');
   });
+
 
 });
 
