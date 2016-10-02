abe8dc2c5b5ef52b682a2b878293f7bf0aa1ea05
Craig Doremus <cdoremus@gmail.com>
2016-10-02 12:31:04 -0400

    Refactored html and css into separate files

--- access-control-routing/src/app/login/login.component.ts
+++ access-control-routing/src/app/login/login.component.ts
@@ -11,67 +11,8 @@ export const authUrl = './users.json';
 
 @Component({
   selector: 'login',
-  styles: [`
-            .login-form-container {
-                margin:5px;
-            }
-            .login-form-font {
-                font-size:20px;
-            }
-            .form-label {
-                display:block;
-                margin-left:10px;
-                margin-right:auto;
-                text-align: left;
-            }
-            legend {
-              margin: 0 0 auto 0;
-              font-size: 30px;
-              text-align:left;
-            }
-            .text-input {
-                width:350px;
-                height:30px;
-            }
-            fieldset {
-                border:0;
-                margin:5px auto;
-            }
-            .form-container {
-                margin:0 auto;
-                width: 400px;
-                border: 2px solid black;
-            }
-            .submit-container {
-                margin:0 auto;
-                width: 70px;
-            }
-            .login-button {
-                margin-bottom: 10px;
-            }
-  `],
-  template: `
-        <div class="login-form-container login-form-font">
-            <form #form="ngForm" (ngSubmit)="onSubmit(form.value)" method="post">
-              <fieldset class="form-container">
-                <legend>Please login</legend>
-                    <fieldset>
-                        <label class="form-label">User name:</label>
-                        <input type="text" class="text-input login-form-font" name="username"
-                          placeholder="Enter user name" tabindex="1" ngModel/>
-                    </fieldset>
-                    <fieldset>
-                        <label class="form-label">Password:</label>
-                        <input type="password" class="text-input login-form-font" name="password"
-                          placeholder="Enter password" tabindex="2" ngModel/>
-                    </fieldset>
-                    <div class="submit-container">
-                        <input type="submit" class="login-button login-form-font" tabindex="3" value="Login" />
-                    </div>
-              </fieldset>
-            </form>
-        </div>
-  `
+  styleUrls: ['./login.component.css'],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
     @Input() username: string;
