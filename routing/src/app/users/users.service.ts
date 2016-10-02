6412370b34022a71ff8875eb9bc5fe738da4a586
Craig Doremus <cdoremus@gmail.com>
2016-10-01 23:51:10 -0400

    Cleaned up a bit

--- routing/src/app/users/users.service.ts
+++ routing/src/app/users/users.service.ts
@@ -6,18 +6,19 @@ import { User } from './user.interfaces';
 const DEBOUNCE_TIME = 2000;
 const USERS_URL = './users.json';
 
-// for use, see SearchbarComponent in autocompletion branch
 @Injectable()
 export class UsersService {
 
   constructor(private http: Http) { }
 
   findUsers(): Observable<any> {
-    return this.http.get(`${USERS_URL}`).flatMap(res => <User[]>res.json());
+    return this.http.get(`${USERS_URL}`)
+      .flatMap(res => <User[]>res.json());
   }
 
   findUserById(id: number): Observable<User> {
-    return this.http.get(`${USERS_URL}`).flatMap(res => <User[]>res.json()).filter(user => user.id == id);
+    return this.findUsers()
+      .filter(user => user.id == id);
   }
 
 }
